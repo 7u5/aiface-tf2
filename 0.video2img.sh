30d6b92949c97a9369926c7c5f6d90fe08e02df1
kliu <qxsy@founder.ai>
2020-09-13 04:49:52 +0800

    change sh

--- 0.video2img.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-vname=$1
-#-s ${h}*${w} 
-vimg=$vname"_keyimgs"
-echo $vimg
-mkdir $vimg
-ffmpeg -i $vname -vf select='eq(pict_type\,I)' -vsync 2 -f image2 $vimg/core-%02d.jpeg
--- 1.sample2tfrecord.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-vname=$1
-source path.sh
-source undo_train.sh $vname
-python image2tfrecord.py $vname
