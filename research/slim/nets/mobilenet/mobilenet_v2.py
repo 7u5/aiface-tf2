9eb95650af0fdfbf5314c640f095718566579ffa
kliu <qxsy@founder.ai>
2020-09-13 23:58:32 +0800

    nasnet

--- research/slim/nets/mobilenet/mobilenet_v2.py
+++ research/slim/nets/mobilenet/mobilenet_v2.py
@@ -32,7 +32,7 @@ import tensorflow as tf
 from nets.mobilenet import conv_blocks as ops
 from nets.mobilenet import mobilenet as lib
 
-slim = tf.contrib.slim
+import tf_slim as slim
 op = lib.op
 
 expand_input = ops.expand_input_by_factor
@@ -98,7 +98,7 @@ def mobilenet(input_tensor,
   Inference mode is created by default. To create training use training_scope
   below.
 
-  with tf.contrib.slim.arg_scope(mobilenet_v2.training_scope()):
+  with tf.slim.arg_scope(mobilenet_v2.training_scope()):
      logits, endpoints = mobilenet_v2.mobilenet(input_tensor)
 
   Args:
@@ -194,7 +194,7 @@ def training_scope(**kwargs):
   """Defines MobilenetV2 training scope.
 
   Usage:
-     with tf.contrib.slim.arg_scope(mobilenet_v2.training_scope()):
+     with tf.slim.arg_scope(mobilenet_v2.training_scope()):
        logits, endpoints = mobilenet_v2.mobilenet(input_tensor)
 
   with slim.
